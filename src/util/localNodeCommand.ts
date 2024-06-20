99aac39333004316749b5d9f4c0fc1b372527d0c
CoNET Project <info@CoNETTech.ca>
2024-06-20 02:31:24 -0700

    added 402 payment error

--- src/util/localNodeCommand.ts
+++ src/util/localNodeCommand.ts
@@ -984,7 +984,7 @@ export const postOpenpgpRouteSocket = async (socket: Socket, headers: string[], 
 
 	if (!payment) {
 		logger(Colors.red(`[${command.walletAddress}] Payment Error!`))
-		return distorySocket(socket, '403')
+		return distorySocket(socket, '402')
 	}
 	logger(Colors.magenta(`${command.walletAddress} passed payment [${payment}] process SaaS!`))
 	return localNodeCommandSocket(socket, headers, command )
