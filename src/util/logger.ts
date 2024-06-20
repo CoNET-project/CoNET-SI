d47b516dc0a09e95470c1decb5a9bb0a11cf7b04
CoNET Project <info@CoNETTech.ca>
2024-06-20 01:26:52 -0700

    bug fix

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,11 +1,13 @@
+import Cluster from 'node:cluster'
 import colors from 'colors/safe'
+const workerNumber = Cluster?.worker?.id ? colors.grey(`worker : ${Cluster.worker.id} `) : `${ Cluster?.isPrimary ? colors.grey('Cluster Master'): colors.bgCyan('Cluster unknow')}`
 //@ts-ignore
 import hexdump from 'hexdump-nodejs'
 
-export  const logger = (...argv: any ) => {
+export const logger = (...argv: any ) => {
     const date = new Date ()
-    const dateStrang = `%c [CONET-worker INFO ${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }:${ date.getMilliseconds ()}]`
-    return console.log ( colors.yellow(`${dateStrang}`),...argv)
+    let dateStrang = `${ workerNumber } [${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }:${ date.getMilliseconds ()}]`
+    return console.log ( colors.yellow(dateStrang), ...argv )
 }
 
 export const hexDebug = ( buffer: Buffer, length: number= 256 ) => {
