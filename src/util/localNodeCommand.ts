8d6424273284d0412cf04c15a6a804c6d49308c2
CoNET Project <info@CoNETTech.ca>
2024-06-21 17:37:10 -0700

    add nodes.isGuardianNode

--- src/util/localNodeCommand.ts
+++ src/util/localNodeCommand.ts
@@ -986,6 +986,7 @@ export const postOpenpgpRouteSocket = async (socket: Socket, headers: string[], 
 		logger(Colors.red(`[${command.walletAddress}] Payment Error!`))
 		return distorySocket(socket, '402 Payment Required')
 	}
+	
 	logger(Colors.magenta(`${command.walletAddress} passed payment [${payment}] process SaaS!`))
 	return localNodeCommandSocket(socket, headers, command )
 	
