9280dee3d7c998aace7f165adb7303c27396a5f6
CoNET Project <info@CoNETTech.ca>
2024-06-23 07:58:43 -0700

    make cluster

--- src/start.ts
+++ src/start.ts
@@ -1,2 +1,12 @@
+import Cluster from 'node:cluster'
+import {cpus} from 'node:os'
 import conet_si_server from './endpoint/server'
-new conet_si_server()
+
+if (Cluster.isPrimary) {
+	for (let i = 0; i < cpus().length; i ++) {
+		Cluster.fork()
+	}
+	
+} else {
+	new conet_si_server()
+}
