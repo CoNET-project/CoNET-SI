29e86e07a4e2ea524a9e2a653e3e12bdbb169914
CoNET Project <info@CoNETTech.ca>
2024-09-06 07:11:27 -0700

    test

--- src/endpoint/sslManager.ts
+++ src/endpoint/sslManager.ts
@@ -1,9 +1,10 @@
 import Express from 'express'
+import { logger } from '../util/logger'
 
 export const startExpressServer = () => {
 	const app = Express()
 	app.use( '/', Express.static(__dirname))
 	app.listen(80)
-	
+	logger(__dirname)
 }
 
