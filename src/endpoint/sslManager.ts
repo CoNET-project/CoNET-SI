a9e99114fa66d474305a22b0a060e63eafef8402
CoNET Project <info@CoNETTech.ca>
2024-09-06 10:36:03 -0700

    test

--- src/endpoint/sslManager.ts
+++ src/endpoint/sslManager.ts
@@ -1,10 +1,29 @@
 import Express from 'express'
+import {createServer, Server, IncomingMessage, ServerResponse} from 'http'
 import { logger } from '../util/logger'
+
+let server: Server<typeof IncomingMessage, typeof ServerResponse> | null = null
+
+export const stopServer = () => new Promise(resolve => {
+	if (!server) {
+		resolve(false)
+		return logger(`startExpressServer server is NULL error!`)
+	}
+
+	server.close(err => {
+		return resolve(true)
+	})
+})
+
 
 export const startExpressServer = () => {
 	const app = Express()
 	app.use( '/', Express.static(__dirname))
-	app.listen(80)
 	logger(__dirname)
+	server = createServer(app)
+	server.listen(80, () => {
+		logger(`startExpressServer server started at 80 port!`)
+	})
+	
 }
 
--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,12 @@
 import Cluster from 'node:cluster'
 import {cpus} from 'node:os'
+
 import conet_si_server from './endpoint/server'
 import {postOpenpgpRouteSocket, IclientPool, generateWalletAddress, getPublicKeyArmoredKeyID, getSetup, loadWalletAddress, makeOpenpgpObj, saveSetup, register_to_DL} from './util/localNodeCommand'
 import {logger} from './util/logger'
 import Colors from 'colors/safe'
 import {exec} from 'node:child_process'
-import {fstat} from 'node:fs'
+import {access, constants} from 'node:fs'
 import {startExpressServer} from './endpoint/sslManager'
 
 process.on ('uncaughtException', (err) => {
@@ -13,26 +14,55 @@ process.on ('uncaughtException', (err) => {
 	console.log("CONET node catch uncaughtException!!!\nNode NOT Exiting...")
 })
 
+const CertificatePATH = ['/etc/letsencrypt/live/slickstack/fullchain.pem','/etc/letsencrypt/live/slickstack/privkey.pem']
 
 if (Cluster.isPrimary) {
 
-
-	const sslCertificate = (keyid: string) => {
+	const _sslCertificate = (keyid: string) => new Promise((resolve, reject) => {
 		const cmd = `sudo certbot certonly -v --noninteractive --agree-tos --cert-name slickstack --register-unsafely-without-email --webroot -w ${__dirname}/endpoint -d ${keyid}.conet.network`
-		exec(cmd, (error, stdout, stderr) => {
+		return exec(cmd, (error, stdout, stderr) => {
 			if (error) {
 				logger(Colors.red(error.message))
+				return reject (error)
 			}
+
 			if (stdout) {
 				logger(Colors.blue(stdout))
 			}
 			if (stderr) {
 				logger(Colors.magenta(stderr))
+				
 			}
+			const cmd1 = `sudo chown peter:peter -R /etc/letsencrypt/`
+			return exec(cmd1, () => {
+				resolve (true)
+			})
+			
 		})
+	})
+		
+	const testCertificateFiles = async () => {
+		await Promise.all([
+			//	@ts-ignore
+			access(CertificatePATH[0], constants.R_OK),
+			//	@ts-ignore
+			access(CertificatePATH[1], constants.R_OK),
+
+		])
 	}
 
+	const sslCertificate = async (publicKeyID: string) => new Promise(async (resolve, reject) => {
+		logger(Colors.magenta(`Didn't init SSL Certificate! Try `))
+		startExpressServer()
+		//await _sslCertificate(publicKeyID)
+		await testCertificateFiles()
+		
+	})
+		
+	
+
 	const start = async () => {
+
 		const initData:ICoNET_NodeSetup|null  = await getSetup ()
 		if ( !initData?.keychain || initData?.passwd === undefined) {
 			throw new Error (`Error: CONET Layer Minus Node have no setup data!\nPlease restart CoNET-SI with command: node dist/cli start!`)
@@ -45,9 +75,7 @@ if (Cluster.isPrimary) {
 		const publicKeyID = initData.pgpKeyObj.publicKeyObj.getKeyIDs()[1].toHex().toUpperCase()
 
 		if (!initData.sslDate) {
-			logger(Colors.magenta(`Didn't init SSL Certificate`))
-			startExpressServer()
-			sslCertificate(publicKeyID)
+			await sslCertificate(publicKeyID)
 		}
 		
 	}
