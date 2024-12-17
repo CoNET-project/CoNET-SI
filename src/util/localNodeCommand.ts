23cba5c3a2276a8f1cf161f847eccdf430d96686
CoNET Project <info@CoNETTech.ca>
2024-12-16 20:56:22 -0800

    Update localNodeCommand.ts

--- src/util/localNodeCommand.ts
+++ src/util/localNodeCommand.ts
@@ -842,7 +842,7 @@ const validatorMining = async (command: minerObj, socket: Socket ) => {
 	}
 
 	const wallet = command.walletAddress.toLowerCase()
-	const message = {epoch: parseInt(validatorData.epoch.toString()), wallet}
+	const message = {epoch: validatorData.epoch, wallet}
 	const nodeWallet = ethers.verifyMessage(JSON.stringify(message), validatorData.hash).toLowerCase()
 
 	if (nodeWallet !== validatorData.nodeWallet.toLowerCase()) {
