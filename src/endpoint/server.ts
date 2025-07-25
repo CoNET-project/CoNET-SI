08bf6596b208dbd40195c2ac8b76980fafa00854
CoNET Project <info@CoNETTech.ca>
2025-07-24 23:50:22 -0700

    Update server.ts

--- src/endpoint/server.ts
+++ src/endpoint/server.ts
@@ -77,21 +77,18 @@ const responseOPTIONS = (socket: Socket, requestHeaders: string[]) => {
 	const acrm = requestHeaders.find(h => h.toLowerCase().startsWith('access-control-request-method:'));
 	const acrmValue = acrm ? acrm.split(/: */, 2)[1].trim() : 'POST';
 
-	const acrh = requestHeaders.find(h => h.toLowerCase().startsWith('access-control-request-headers:'));
-	const acrhValue = acrh ? acrh.split(/: */, 2)[1].trim() : 'content-type';
-
 	const response = [
 		'HTTP/1.1 204 No Content',
 		`Access-Control-Allow-Origin: ${origin}`,
 		`Access-Control-Allow-Methods: ${acrmValue}, OPTIONS`,
-		`Access-Control-Allow-Headers: ${acrhValue}`,
+		'Access-Control-Allow-Headers: solana-client, DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type',
 		'Access-Control-Max-Age: 86400',
 		'Content-Length: 0',
 		'Connection: keep-alive',
-		'', // <- 这两个空行是关键！形成 \r\n\r\n
+		'', // \r\n 结尾
 		''
 	].join('\r\n');
-	logger(`${response}`)
+
 	socket.end(response);
 };
 
