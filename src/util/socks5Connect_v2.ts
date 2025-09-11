db68ad9b75c959b52957ad7a73da57c381c4e3ec
CoNET Project <info@CoNETTech.ca>
2025-09-10 23:27:10 -0700

    Update socks5Connect_v2.ts

--- src/util/socks5Connect_v2.ts
+++ src/util/socks5Connect_v2.ts
@@ -25,7 +25,7 @@ class BandwidthCount extends Transform {
             this.startTime = Date.now()
         }
         this.count += chunk.length
-        //logger(`${this.tab} start at ${this.startTime} BandwidthCount ${this.count} bytes`)
+        logger(`${this.tab} start at ${this.startTime} BandwidthCount ${this.count} bytes`)
         this.push(chunk)
         callback()
     }
